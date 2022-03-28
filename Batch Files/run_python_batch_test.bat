1185fb5c79943975bad6156919cc6f7906b2f3be
Atikah <atikah.hussain@hotmail.co.uk>
2022-03-28 17:37:12 +0100

    Update run_python_batch_test.bat

--- Batch Files/run_python_batch_test.bat
+++ Batch Files/run_python_batch_test.bat
@@ -1,6 +1,5 @@
 @ECHO off
 set batch_test="C:\Users\atika\Documents\GitHub\numerical_computing\Batch Files\batch_test.py"
-python %batch_test% "Atikah" "230101" 
-::"E3 2ZB"
+python %batch_test% "Atikah" "230101" "AX 2ZB"
 PAUSE
 
